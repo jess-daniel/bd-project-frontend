708dc612d05ea1e69b2f0c96b81baddc3bdc88a8
Jess Daniel <jessdaniel474@gmail.com>
2020-04-11 15:57:17 -0400

    reorganized nav components to be more modular

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,28 +4,24 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+
+// local imports
+import MainNav from './MainNav';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  button: {
-    color: 'white',
-    textDecoration: 'none',
-    '&:hover': {
-      backgroundColor: 'white',
-      color: 'black',
-    },
+  bar: {
+    display: 'flex',
+    justifyContent: 'space-between',
   },
   link: {
     textDecoration: 'none',
+    color: 'white',
   },
   title: {
-    flexGrow: 1,
+    width: '20%',
   },
 }));
 
@@ -35,15 +31,13 @@ const NavBar = () => {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar className={classes.bar}>
           <Typography variant="h6" className={classes.title}>
-            Message Thing
+            <Link to="/" className={classes.link}>
+              Message Thing
+            </Link>
           </Typography>
-          <Link to="/add-message" className={classes.link}>
-            <Button className={classes.button} color="inherit">
-              Add A Message!
-            </Button>
-          </Link>
+          <MainNav />
         </Toolbar>
       </AppBar>
     </div>
